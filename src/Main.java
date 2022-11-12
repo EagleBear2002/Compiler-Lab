7c25b9e9bc1788169c96abd3cfe0846cfaabb718
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-12 12:23:35 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  12:23:35 up 20 min,  0 users,  load average: 0.52, 0.58, 0.59 b75994c616174e2a35d547eeeb327288f5de9e64

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) return;
+		String path = args[0];
+		File file = new File(path);
+		FileInputStream fileStream = new FileInputStream(file);
+		InputStreamReader inReader = new InputStreamReader(fileStream, "UTF-8");
+		BufferedReader bfReader = new BufferedReader(inReader);
+		String contentLine = "";
+		while ((contentLine = bfReader.readLine()) != null) {
+			System.out.println(contentLine);
+		}
 	}
 }
