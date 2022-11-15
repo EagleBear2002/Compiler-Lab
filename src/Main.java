764a43734f33c7cc77351ceeb6811cff12d5ac14
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-16 00:11:17 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  00:11:17 up 20 min,  0 users,  load average: 0.52, 0.58, 0.59 9e3f7cd377d8b1853c51e07f6ab9582cb2a3862

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
 			System.err.println("input path is required");
+			return;
 		}
+		
 		String sourcePath = args[0];
 		CharStream input = CharStreams.fromFileName(sourcePath);
 		SysYLexer sysYLexer = new SysYLexer(input);
