82b393ae3d538690204bf89c81406c6b66e3e8d5
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-26 16:20:26 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  16:20:26 up 1 day, 20:52,  0 users,  load average: 0.52, 0.58, 0.59 32e503414548581526a57641d12bf72f011ab54

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 import java.io.*;
 import java.util.List;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
 			System.err.println("input path is required");
@@ -14,6 +16,7 @@ public class Main {
 		
 		String sourcePath = args[0];
 		CharStream input = CharStreams.fromFileName(sourcePath);
+		
 		SysYLexer sysYLexer = new SysYLexer(input);
 		
 		MyErrorListener myErrorListener = new MyErrorListener();
@@ -38,7 +41,12 @@ public class Main {
 				}
 			}
 			
-			System.err.printf("%s %s at Line %d.\n", ruleNames[ruleNum-1], tokenText, lineNum);
+			System.err.printf("%s %s at Line %d.\n", ruleNames[ruleNum - 1], tokenText, lineNum);
+			
 		}
+		
+		CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+		SysYParser sysYParser = new SysYParser(tokens);
+		
 	}
 }
