59a48918a7776e0e7cd7c1fdde6fceb90421482c
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-28 09:26:17 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  09:26:17 up 3 days, 13:58,  0 users,  load average: 0.52, 0.58, 0.59 30e499c21313dcb5a2de0b53a802f438f15248bc

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,8 @@ import org.antlr.v4.runtime.Token;
 import java.io.*;
 import java.util.List;
 
-public class Main {	
-	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
-			System.err.println("input path is required");
-			return;
-		}
-		
-// Lexer
-		String sourcePath = args[0];
+public class Main {
+	public static SysYLexer lexer(String sourcePath) throws IOException {
 		CharStream input = CharStreams.fromFileName(sourcePath);
 		
 		SysYLexer sysYLexer = new SysYLexer(input);
@@ -23,11 +16,11 @@ public class Main {
 		sysYLexer.removeErrorListeners();
 		sysYLexer.addErrorListener(myLexerErrorListener);
 		
-		if (myLexerErrorListener.listenError()) {
-			return;
-		}
-		
-		List<? extends Token> tokenList = sysYLexer.getAllTokens();
+//		if (myLexerErrorListener.listenError()) {
+//			return sysYLexer;
+//		}
+//		
+//		List<? extends Token> tokenList = sysYLexer.getAllTokens();
 //		String[] ruleNames = sysYLexer.getRuleNames();
 //		for (Token token : tokenList) {
 //			String tokenText = token.getText();
@@ -44,7 +37,10 @@ public class Main {
 //			System.err.printf("%s %s at Line %d.\n", ruleNames[ruleNum - 1], tokenText, lineNum);
 //		}
 		
-// Parser
+		return sysYLexer;
+	}
+	
+	public static SysYParser parser(SysYLexer sysYLexer) {
 		CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
 		SysYParser sysYParser = new SysYParser(tokens);
 		MyParserErrorListener myParserErrorListener = new MyParserErrorListener();
@@ -52,8 +48,22 @@ public class Main {
 		sysYParser.addErrorListener(myParserErrorListener);
 		
 		if (myParserErrorListener.listenError()) {
+			return sysYParser;
+		}
+		
+		
+		
+		return sysYParser;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			System.err.println("input path is required");
 			return;
 		}
 		
+		SysYLexer sysYLexer = lexer(args[0]);
+
+		SysYParser sysYParser = parser(sysYLexer);
 	}
 }
