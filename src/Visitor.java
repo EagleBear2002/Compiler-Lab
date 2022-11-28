edbbe930ec8d8b6326d3ed1e9ada117f563146f7
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-28 11:27:15 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  11:27:15 up 3 days, 15:59,  0 users,  load average: 0.52, 0.58, 0.59 618ab1e2be8fbd8f9b230e7c5aec9d0937d715d

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,9 +1,14 @@
+import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Visitor extends SysYParserBaseVisitor<Void>{
 	@Override
 	public Void visitChildren(RuleNode node) {
+		RuleContext ctx = node.getRuleContext();
+		int id = ctx.getRuleIndex();
+		String ruleName = SysYParser.ruleNames[id];
+		System.err.printf("visitChildren(%s)\n", ruleName);
 		return super.visitChildren(node);
 	}
 	
