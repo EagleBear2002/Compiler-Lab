d7141d908df083bb75eb004d05fc228650ff0491
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-28 11:42:13 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  11:42:13 up 3 days, 16:14,  0 users,  load average: 0.52, 0.58, 0.59 41efb9d58efc9760ad86620d310d1ff627d76f31

--- src/Visitor.java
+++ src/Visitor.java
@@ -2,16 +2,23 @@ import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class Visitor extends SysYParserBaseVisitor<Void>{
+public class Visitor extends SysYParserBaseVisitor<Void> {
+	private static int depth = 0;
 	@Override
 	public Void visitChildren(RuleNode node) {
 		RuleContext ctx = node.getRuleContext();
 		int id = ctx.getRuleIndex();
 		String ruleName = SysYParser.ruleNames[id];
 		String realName = ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
+		
+		for (int i = 0; i < depth; ++i)
+			System.err.print("  ");
 		System.err.printf("visitChildren(%s)\n", realName);
 		
-		return super.visitChildren(node);
+		depth++;
+		Void ret = super.visitChildren(node);
+		depth--;
+		return ret;
 	}
 	
 	@Override
