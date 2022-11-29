5ea05f800e991620d3eea740e7c79497dab6fc24
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-29 18:15:01 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  18:15:01 up  1:02,  0 users,  load average: 0.52, 0.58, 0.59 3a5a1b8a7d8944a949ff96a97815be25bdf3073c

--- src/Main.java
+++ src/Main.java
@@ -41,113 +41,59 @@ public class Main {
 	}
 
 	public static SysYParser parser(SysYLexer sysYLexer) {
+		CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+		SysYParser sysYParser = new SysYParser(tokens);
+		MyParserErrorListener myParserErrorListener = new MyParserErrorListener();
+		sysYParser.removeErrorListeners();
+		sysYParser.addErrorListener(myParserErrorListener);
+
+		ParseTree tree = sysYParser.program();
+		System.err.println("Parser Checking");
+		if (myParserErrorListener.listenError()) {
+			return sysYParser;
+		}
+		System.err.println("Successful Checking");
+
+		// Visitor extends SysYParserBaseVisitor<Void>
+		Visitor visitor = new Visitor();
+		visitor.visit(tree);
 		
+		return sysYParser;
+	}
+
+	public static void main(String[] args) throws IOException {
+//		if (args.length < 1) {
+//			System.err.println("input path is required");
+//			return;
+//		}
+//
+//		SysYLexer sysYLexer = lexer(args[0]);
+//		SysYParser sysYParser = parser(sysYLexer);
+		
+		
+		if (args.length < 1) {
+			System.err.println("input path is required");
+		}
+		String source = args[0];
+		CharStream input = CharStreams.fromFileName(source);
+		
+		SysYLexer sysYLexer = new SysYLexer(input);
+		sysYLexer.removeErrorListeners();
+		MyLexerErrorListener myErrorListener = new MyLexerErrorListener();
+		sysYLexer.addErrorListener(myErrorListener);
 		CommonTokenStream commonTokenStream = new CommonTokenStream(sysYLexer);
 		SysYParser sysYParser = new SysYParser(commonTokenStream);
 		sysYParser.removeErrorListeners();
 		MyParserErrorListener parserErrorListener = new MyParserErrorListener();
 		sysYParser.addErrorListener(parserErrorListener);
 		ParseTree tree = sysYParser.program();
-		if(parserErrorListener.listenError()){
-			return sysYParser;
+		if (parserErrorListener.listenError()) {
+			return;
 		}
 		Visitor visitor = new Visitor();
 		String[] s = sysYLexer.getRuleNames();
 		
-//		CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
-//		SysYParser sysYParser = new SysYParser(tokens);
-//		MyParserErrorListener myParserErrorListener = new MyParserErrorListener();
-//		sysYParser.removeErrorListeners();
-//		sysYParser.addErrorListener(myParserErrorListener);
-//
-//		ParseTree tree = sysYParser.program();
-//		System.err.println("Parser Checking");
-//		if (myParserErrorListener.listenError()) {
-//			return sysYParser;
-//		}
-//		System.err.println("Successful Checking");
-//		
-//		// Visitor extends SysYParserBaseVisitor<Void>
-//		Visitor visitor = new Visitor();
-//		visitor.visit(tree);
-		
-		return sysYParser;
-	}
-
-	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
-			System.err.println("input path is required");
-			return;
-		}
-
-		SysYLexer sysYLexer = lexer(args[0]);
-		SysYParser sysYParser = parser(sysYLexer);
+		visitor.setTokenNames(s, sysYParser.getRuleNames());
+		visitor.visit(tree);
 	}
 }
-//import org.antlr.v4.runtime.*;
-//import org.antlr.v4.runtime.tree.ParseTree;
-//
-//import java.io.*;
-//
-//public class Main {
-//	public static void main(String[] args) throws IOException {
-//		if (args.length < 1) {
-//			System.err.println("input path is required");
-//		}
-//		String source = args[0];
-//		CharStream input = CharStreams.fromFileName(source);
-////        CharStream input = CharStreams.fromFileName("tests/test1.sysy");
-//		SysYLexer sysYLexer = new SysYLexer(input);
-//		sysYLexer.removeErrorListeners();
-//		MyLexerErrorListener myErrorListener = new MyLexerErrorListener();
-//		sysYLexer.addErrorListener(myErrorListener);
-//		
-//		CommonTokenStream commonTokenStream = new CommonTokenStream(sysYLexer);
-//		SysYParser sysYParser = new SysYParser(commonTokenStream);
-//		sysYParser.removeErrorListeners();
-//		MyParserErrorListener parserErrorListener = new MyParserErrorListener();
-//		sysYParser.addErrorListener(parserErrorListener);
-//		ParseTree tree = sysYParser.program();
-//		if(parserErrorListener.listenError()){
-//			return;
-//		}
-//		Visitor visitor = new Visitor();
-//		String[] s = sysYLexer.getRuleNames();
-////        for(int i = 0; i < sysYLexer.getRuleNames().length; i++){
-////            System.err.println(s[i]);
-////        }
-////		visitor.setTokenNames(s, sysYParser.getRuleNames());
-////		visitor.visit(tree);
-//
-////
-//
-////        for(Token a : atn){
-////            System.err.print(name[a.getType()-1]);
-////            System.err.print(' ');
-////            if(name[a.getType()-1].equals("INTEGR_CONST")) {
-////                String s = String.valueOf(a.getText());
-////                if(s.length() < 2){
-////                    System.err.print(s);
-////                }
-////                else if(s.charAt(0) == '0' && s.charAt(1) == 'x'){
-////                    int x=Integer.parseInt(s.substring(2),16);
-////                    System.err.print(x);
-////                }
-////                else if(s.charAt(0) == '0'){
-////                    int x=Integer.parseInt(s.substring(1), 8);
-////                    System.err.print(x);
-////                }
-////                else {
-////                    System.err.print(a.getText());
-////                }
-////            }
-////            else {
-////                System.err.print(a.getText());
-////            }
-////            System.err.print(" at Line ");
-////            System.err.print(a.getLine());
-////            System.err.println('.');
-////
-////        }
-//	}
-//}
