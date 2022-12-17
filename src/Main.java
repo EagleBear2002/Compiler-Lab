edb94611b612ca094492b55c8578051d36834fbe
EagleBear2002 <eaglebear@smail.nju.edu.cn>
2022-12-17 17:36:49 +0800

    refactor the project

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import Lexer.SysYLexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import Parser.MyParserErrorListener;
-import Parser.SysYParser;
-import Parser.Visitor;
 
 import java.io.*;
 
@@ -38,34 +34,34 @@ public class Main {
 //
 ////			System.err.printf("%s %s at Line %d.\n", ruleNames[ruleNum - 1], tokenText, lineNum);
 //		}
-
+		
 		return sysYLexer;
 	}
-
+	
 	public static SysYParser parser(SysYLexer sysYLexer) {
 		CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
 		SysYParser sysYParser = new SysYParser(tokens);
 		MyParserErrorListener myParserErrorListener = new MyParserErrorListener();
 		sysYParser.removeErrorListeners();
 		sysYParser.addErrorListener(myParserErrorListener);
-
+		
 		ParseTree tree = sysYParser.program();
 		if (myParserErrorListener.listenError()) {
 			return sysYParser;
 		}
-
+		
 		Visitor visitor = new Visitor();
 		visitor.visit(tree);
 		
 		return sysYParser;
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
 			System.err.println("input path is required");
 			return;
 		}
-
+		
 		String filePath = args[0];
 		int lineNO = Integer.valueOf(args[1]);
 		int column = Integer.valueOf(args[2]);
---
+++ src/MyParserErrorListener.java
@@ -0,0 +1,34 @@
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+import java.util.BitSet;
+
+public class MyParserErrorListener extends BaseErrorListener {
+	private boolean isError = false;
+	public boolean listenError() {
+		return isError;
+	}
+	
+	
+	@Override
+	public void syntaxError(Recognizer<?, ?> recognizer, Object o, int lineNum, int posInLine, String msg, RecognitionException e) {
+		isError = true;
+		System.err.printf("Error type B at Line %d: %s\n", lineNum, msg);
+	}
+	
+	@Override
+	public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+	}
+	
+	@Override
+	public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+	}
+	
+	@Override
+	public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+	}
+}
