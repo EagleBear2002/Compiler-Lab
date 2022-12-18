3770142af39ac40fbc5ed1ca171a3edb9d19ecb8
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-18 19:00:17 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  19:00:17 up 19:31,  0 users,  load average: 0.52, 0.58, 0.59 4eb24dda1c3a12e9d72dcc9ab09c36e2f1f688e0

--- src/Visitor.java
+++ src/Visitor.java
@@ -14,22 +14,22 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 	private int renameColumnNo;
 	private String newName;
 	private boolean isPrint;
-	
+
 	void setPrint(boolean isPrint) {
 		this.isPrint = isPrint;
 	}
-	
+
 	void setRenameTag(int renameLineNo, int renameColumnNo, String newName) {
 		this.renameLineNo = renameLineNo;
 		this.renameColumnNo = renameColumnNo;
 		this.newName = newName;
 	}
-	
+
 	private void printIdent(int depth) {
 		for (int i = 0; i < depth; ++i)
 			System.err.print("  ");
 	}
-	
+
 	private String getHelight(String ruleName) {
 		switch (ruleName) {
 			case "CONST":
@@ -43,7 +43,7 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 			case "RETURN": {
 				return "orange";
 			}
-			
+
 			case "PLUS":
 			case "MINUS":
 			case "MUL":
@@ -61,50 +61,50 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 			case "OR": {
 				return "blue";
 			}
-			
+
 			case "IDENT": {
 				return "red";
 			}
-			
+
 			case "INTEGR_CONST": {
 				return "green";
 			}
-			
+
 			default: {
 				return "no color";
 			}
 		}
 	}
-	
+
 	@Override
 	public Void visitChildren(RuleNode node) {
 		RuleContext ctx = node.getRuleContext();
 		int ruleIndex = ctx.getRuleIndex();
 		String ruleName = SysYParser.ruleNames[ruleIndex];
 		String realName = ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
-		
+
 		if (isPrint) {
 			printIdent(depth);
 			System.err.println(realName);
 		}
-		
+
 		depth++;
 		Void ret = super.visitChildren(node);
 		depth--;
-		
+
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitTerminal(TerminalNode node) {
 		Token token = node.getSymbol();
 		int ruleNum = token.getType() - 1;
-		
+
 		if (ruleNum >= 0) {
 			String ruleName = SysYLexer.ruleNames[ruleNum];
 			String tokenText = token.getText();
 			String color = getHelight(ruleName);
-			
+
 			if (ruleName == "INTEGR_CONST") {
 				if (tokenText.startsWith("0x") || tokenText.startsWith("0X")) {
 					tokenText = String.valueOf(Integer.parseInt(tokenText.substring(2), 16));
@@ -112,66 +112,66 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 					tokenText = String.valueOf(Integer.parseInt(tokenText, 8));
 				}
 			}
-			
+
 			if (ruleName == "IDENT") {
 				int lineNO = token.getLine();
 				int columnNO = token.getCharPositionInLine();
 				Symbol symbol = currentScope.resolve(tokenText);
 				if (symbol != null) {
 					symbol.addUsage(lineNO, columnNO);
-					
+
 					if (symbol.findUsage(renameLineNo, renameColumnNo)) {
-						System.out.println("tokenText = " + tokenText + ", findUsage(" + renameLineNo + ", " + renameColumnNo + ")");
+						System.out.println("tokenText = " + tokenText + ", findUsage(" + renameLineNo + ", "
+								+ renameColumnNo + ")");
 						tokenText = newName;
 					}
 				}
 			}
-			
+
 			if (isPrint && color != "no color") {
 				printIdent(depth);
 				System.err.println(tokenText + " " + ruleName + "[" + color + "]");
 			}
 		}
-		
+
 		Void ret = super.visitTerminal(node);
 		return ret;
 	}
-	
-	
+
 	@Override
 	public Void visit(ParseTree tree) {
 		Void ret = super.visit(tree);
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitProgram(SysYParser.ProgramContext ctx) {
 		globalScope = new GlobalScope(null);
 		currentScope = globalScope;
 		Void ret = super.visitProgram(ctx);
 		currentScope = currentScope.getEnclosingScope();
-		
+
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitFuncDef(SysYParser.FuncDefContext ctx) {
 		String typeName = ctx.funcType().getText();
 		globalScope.resolve(typeName);
-		
+
 		String funcName = ctx.IDENT().getText();
 		FunctionSymbol fun = new FunctionSymbol(funcName, currentScope);
-		
+
 		currentScope.define(fun);
 		currentScope = fun;
-		
+
 		Void ret = super.visitFuncDef(ctx);
-		
+
 		currentScope = currentScope.getEnclosingScope();
-		
+
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitBlock(SysYParser.BlockContext ctx) {
 		LocalScope localScope = new LocalScope(currentScope);
@@ -179,42 +179,46 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 		localScope.setName(localScopeName);
 		localScopeCounter++;
 		currentScope = localScope;
-		
+
 		Void ret = super.visitBlock(ctx);
 		currentScope = currentScope.getEnclosingScope();
-		
+
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitVarDecl(SysYParser.VarDeclContext ctx) {
 		String typeName = ctx.bType().getText();
 		Type type = (Type) globalScope.resolve(typeName);
-		
+
 		for (SysYParser.VarDefContext varDefContext : ctx.varDef()) {
 			String varName = varDefContext.IDENT().getText();
+			if (currentScope.resolve(varName)) {
+				int lineNo = ctx.IDENT().getSymbol().getLine();
+				System.err.println("Error type 3 at Line " + lineNo + ": Undefined variable: " + varName + ".");
+			}
 			VariableSymbol varSymbol = new VariableSymbol(varName, type);
 			currentScope.define(varSymbol);
 		}
-		
+
 		Void ret = super.visitVarDecl(ctx);
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitFuncFParam(SysYParser.FuncFParamContext ctx) {
 		String typeName = ctx.bType().getText();
 		Type type = (Type) globalScope.resolve(typeName);
-		
+
 		String varName = ctx.IDENT().getText();
 		VariableSymbol varSymbol = new VariableSymbol(varName, type);
-		
+
 		currentScope.define(varSymbol);
-		
+
 		Void ret = super.visitFuncFParam(ctx);
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitLVal(SysYParser.LValContext ctx) {
 		String varName = ctx.IDENT().getText();
@@ -225,13 +229,13 @@ public class Visitor extends SysYParserBaseVisitor<Void> {
 		Void ret = super.visitLVal(ctx);
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitStmt(SysYParser.StmtContext ctx) {
 		Void ret = super.visitStmt(ctx);
 		return ret;
 	}
-	
+
 	@Override
 	public Void visitExp(SysYParser.ExpContext ctx) {
 		if (ctx.IDENT() != null) {
