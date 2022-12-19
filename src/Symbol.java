9c2d4a8f494d1faec8322556184e03e85b283b14
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-19 19:14:13 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  19:14:13 up 1 day, 19:44,  0 users,  load average: 0.52, 0.58, 0.59 3f5ec415aa679e5e27fab43d1d3049abef1cd20

--- src/Symbol.java
+++ src/Symbol.java
@@ -5,21 +5,11 @@ import java.util.List;
 
 public interface Symbol {
 	
-	public String getName();
+	String getName();
 	
-	public void addUsage(int lineNo, int columnNo);
+	void addUsage(int lineNo, int columnNo);
 	
-	public boolean findUsage(int lineNo, int columnNo);
+	boolean findUsage(int lineNo, int columnNo);
 	
-	public Type getType();
-//	public default void addUsage(int lineNo, int columnNo) {
-//		System.out.println("addUsage(" + lineNo + ", " + columnNo + ", " + getName() + ")");
-//		System.out.println("size = " + usagePosition.size());
-//		usagePosition.add(new Pair(lineNo, columnNo));
-//	}
-//	
-//	public default boolean findUsage(int lineNo, int columnNo) {
-//		boolean ret = usagePosition.contains(new Pair(lineNo, columnNo));
-//		return ret;
-//	}
+	Type getType();
 }
