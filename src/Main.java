5e56236be681fd2bd885482f3ddbe002b6b6270c
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-25 12:27:25 +0800

    > make 201250172 eaglebear2002 Linux EagleBear2002-HP 4.4.0-19041-Microsoft #2311-Microsoft Tue Nov 08 17:09:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  12:27:25 up 21 min,  0 users,  load average: 0.52, 0.58, 0.59 31422c58e526d5748524bf2d7d06fad4b449af3d

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.bytedeco.llvm.LLVM.*;
+import static org.bytedeco.llvm.global.LLVM.*;
 
-import java.io.*;
+import java.io.IOException;
 
 public class Main {
+	public static final BytePointer error = new BytePointer();
+	
 	public static SysYLexer lexer(String sourcePath) throws IOException {
 		CharStream input = CharStreams.fromFileName(sourcePath);
 		return new SysYLexer(input);
@@ -22,29 +26,18 @@ public class Main {
 			return;
 		}
 		
-		String filePath = args[0];
-		int lineNo = Integer.parseInt(args[1]);
-		int columnNo = Integer.parseInt(args[2]);
-		String name = args[3];
-		SysYLexer sysYLexer = lexer(filePath);
+		String srcPath = args[0];
+		String destPath = args[0];
+		SysYLexer sysYLexer = lexer(srcPath);
 		SysYParser sysYParser = parser(sysYLexer);
 		
 		ParseTree tree = sysYParser.program();
 		Visitor visitor = new Visitor();
 		visitor.visit(tree);
 		
-		if (!visitor.getErrorFound()) {
-			for (Object obj : visitor.getMsgToPrint()) {
-				if (obj instanceof Symbol) {
-					if (((Symbol) obj).findUsage(lineNo, columnNo)) {
-						System.err.print(name);
-					} else {
-						System.err.print(((Symbol) obj).getName());
-					}
-				} else {
-					System.err.print(obj);
-				}
-			}
-		}
+		
+//		if (LLVMPrintModuleToFile(moudule, args[1], error) != 0) {    // moudle是你自定义的LLVMModuleRef对象
+//			LLVMDisposeMessage(error);
+//		}
 	}
 }
