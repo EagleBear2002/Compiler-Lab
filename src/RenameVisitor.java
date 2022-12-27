e52631a2dd057294cbdec15fd993e4ad172bfa94
EagleBear2002 <eaglebear@smail.nju.edu.cn>
2022-12-27 17:42:16 +0800

    refract the project

--- src/RenameVisitor.java
+++ src/RenameVisitor.java
@@ -4,7 +4,6 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
-import antlr.*;
 
 import Symbol.*;
 import Scope.*;
