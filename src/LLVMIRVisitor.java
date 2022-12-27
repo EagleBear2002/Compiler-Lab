20a93cfc771008250370155a243222773219db9d
EagleBear2002 <eaglebear@smail.nju.edu.cn>
2022-12-27 19:12:20 +0800

    pass test1

--- src/LLVMIRVisitor.java
+++ src/LLVMIRVisitor.java
@@ -8,15 +8,7 @@ import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.PointerPointer;
 import org.bytedeco.llvm.LLVM.*;
 
-import Symbol.*;
-import Scope.*;
-import Type.*;
-
 import static org.bytedeco.llvm.global.LLVM.*;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Stack;
 
 public class LLVMIRVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
 	// 创建 module
@@ -27,7 +19,6 @@ public class LLVMIRVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
 	
 	// 考虑到我们的语言中仅存在 int 一个基本类型，可以通过下面的语句为 LLVM 的 int 型重命名方便以后使用
 	private final LLVMTypeRef i32Type = LLVMInt32Type();
-	private final Stack<LLVMValueRef> operandStack = new Stack<>();
 	
 	public LLVMIRVisitor() {
 		// 初始化 LLVM
@@ -42,15 +33,15 @@ public class LLVMIRVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
 		return module;
 	}
 	
-	@Override
-	public LLVMValueRef visitChildren(RuleNode node) {
-		RuleContext ctx = node.getRuleContext();
-		int ruleIndex = ctx.getRuleIndex();
-		String ruleName = SysYParser.ruleNames[ruleIndex];
-		String realName = ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
-		
-		return super.visitChildren(node);
-	}
+//	@Override
+//	public LLVMValueRef visitChildren(RuleNode node) {
+//		RuleContext ctx = node.getRuleContext();
+//		int ruleIndex = ctx.getRuleIndex();
+//		String ruleName = SysYParser.ruleNames[ruleIndex];
+//		String realName = ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
+//		
+//		return super.visitChildren(node);
+//	}
 	
 	private String toDecimalInteger(String tokenText) {
 		if (tokenText.startsWith("0x") || tokenText.startsWith("0X")) {
@@ -63,80 +54,61 @@ public class LLVMIRVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
 	
 	@Override
 	public LLVMValueRef visitTerminal(TerminalNode node) {
-		Token token = node.getSymbol();
-		int ruleNum = token.getType() - 1;
-		
-		if (ruleNum < 0) {
-			return super.visitTerminal(node);
+		Token symbol = node.getSymbol();
+		int symbolType = symbol.getType();
+
+		if (symbolType == SysYParser.INTEGR_CONST) {
+			int number = Integer.parseInt(toDecimalInteger(node.getText()));
+			return LLVMConstInt(i32Type, number, 1);
 		}
 		
 		return super.visitTerminal(node);
 	}
 	
 	@Override
-	public LLVMValueRef visit(ParseTree tree) {
-		return super.visit(tree);
-	}
-	
-	@Override
-	public LLVMValueRef visitProgram(SysYParser.ProgramContext ctx) {
-		LLVMValueRef ret = super.visitProgram(ctx);
-		return ret;
-	}
-	
-	@Override
 	public LLVMValueRef visitFuncDef(SysYParser.FuncDefContext ctx) {
-		LLVMTypeRef returnType = i32Type;
+		LLVMTypeRef functionType = LLVMFunctionType(i32Type, LLVMVoidType(), 0, 0);
+		String funtionName = ctx.IDENT().getText();
+		LLVMValueRef funtion = LLVMAddFunction(module, funtionName, functionType);
 		
-		LLVMTypeRef functionType = LLVMFunctionType(returnType, LLVMVoidType(), 0, 0);
+		LLVMBasicBlockRef mainEntry = LLVMAppendBasicBlock(funtion, "mainEntry");
+		LLVMPositionBuilderAtEnd(builder, mainEntry);
+		super.visitFuncDef(ctx);
 		
-		LLVMValueRef main = LLVMAddFunction(module, "main", functionType);
-		
-		operandStack.push(main);
-		
-		visitBlock(ctx.block());
-		
-		operandStack.pop();
-		
+		return funtion;
+	}
+
+	@Override
+	public LLVMValueRef visitUnaryExp(SysYParser.UnaryExpContext ctx) {
+		String op = ctx.unaryOp().getText();
+
+		LLVMValueRef expValue = visit(ctx.exp());
+
+		switch (op) {
+			case "+" -> {
+				return expValue;
+			}
+			case "-" -> {
+				return LLVMBuildNeg(builder, expValue, "tmp_");
+			}
+			case "!" -> {
+				long value = LLVMConstIntGetZExtValue(expValue);
+				if (value == 0) {
+					return LLVMConstInt(i32Type, 1, 1);
+				} else {
+					return LLVMConstInt(i32Type, 0, 1);
+				}
+			}
+			default -> {
+			}
+		}
+
 		return null;
 	}
-	
-//	@Override
-//	public LLVMValueRef visitUnaryExp(SysYParser.UnaryExpContext ctx) {
-//		String op = ctx.unaryOp().getText();
-//		
-//		LLVMValueRef expValue = visit(ctx.exp());
-//		
-//		switch (op) {
-//			case "+" -> {
-//				return expValue;
-//			}
-//			case "-" -> {
-//				return LLVMBuildNeg(builder, expValue, "tmp_");
-//			}
-//			case "!" -> {
-//				long value = LLVMConstIntGetZExtValue(expValue);
-//				if (value == 0) {
-//					return LLVMConstInt(i32Type, 1, 1);
-//				} else {
-//					return LLVMConstInt(i32Type, 0, 1);
-//				}
-//			}
-//			default -> {
-//			}
-//		}
-//		
-//		return null;
-//	}
 	
 	@Override
 	public LLVMValueRef visitReturnStmt(SysYParser.ReturnStmtContext ctx) {
 		LLVMValueRef result = visit(ctx.exp());
-		LLVMBasicBlockRef mainEntry = LLVMAppendBasicBlock(operandStack.peek(), "mainEntry");
-		LLVMPositionBuilderAtEnd(builder, mainEntry);
-		
-		LLVMBuildRet(builder, result);
-		
-		return null;
+		return LLVMBuildRet(builder, result);
 	}
 }
