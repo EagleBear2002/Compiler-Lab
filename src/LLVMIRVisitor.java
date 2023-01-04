f6777e51bca18e047547c853f6ad00d92c43f392
EagleBear2002 <eaglebear@smail.nju.edu.cn>
2023-01-04 19:14:07 +0800

    fix visitFuncCallExp

--- src/LLVMIRVisitor.java
+++ src/LLVMIRVisitor.java
@@ -264,7 +264,7 @@ public class LLVMIRVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
 				args.put(i, this.visit(expContext));
 			}
 		}
-		return LLVMBuildCall(builder, function, args, argsCount, "call_" + functionName);
+		return LLVMBuildCall(builder, function, args, argsCount, "");
 	}
 	
 	@Override
